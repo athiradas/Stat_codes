b2a479dbddb03af10f06125e5dd6f4e2be817dda
Athira Das <athiradas.mec@gmail.com>
2017-10-26 17:17:06 -0500

    Add files via upload

---
+++ Outlier_Detection.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 17:15:23 2017
+
+@author: athir
+"""
+
+def get_stats(pro_code):
+    conn_str = (
+        '
+        )
+    id_values = {}                    
+    cnxn = pyodbc.connect(conn_str)
+    cursor = cnxn.cursor()
+    cursor.execute("""Query to fetch data""")
+    rows = np.asarray(cursor.fetchall())
+    data = rows[:,1].astype(np.float)  
+    id_values['data'] =  data
+    id_values['mean'] =  np.mean(data)
+    id_values['std'] =  np.std(data)
+    id_values['skew'] =  scipy.stats.skew(data)
+    id_values['kurtosis'] =  scipy.stats.kurtosis(data)
+    id_values['hist']= np.hstack(data)   
+    p1 = 0.01
+    k = 1/sqrt(p1)
+    print(k)
+    odvu = id_values['mean'] + k*id_values['std']
+    odvl = id_values['mean'] - k*id_values['std']
+    id_values['upper_cut']= odvu
+    id_values['outliers']= data[np.where( data > odvu )] 
+    id_values['outlier percentage'] =  id_values['outliers'].shape[0]/id_values['data'].shape[0]*100
+    cnxn.close() 
+    return id_values
